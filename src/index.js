f3c66166e5e888a6bf16b54d4570e668a39b1fad
Namung Kim <knw1121@gmail.com>
2018-05-27 16:34:27 +0900

    Initialized firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyC8L-IC-hN1-5TDxemn91lTMEc4RWHkTsI",
+  authDomain: "osori-7e197.firebaseapp.com",
+  databaseURL: "https://osori-7e197.firebaseio.com",
+  projectId: "osori-7e197",
+  storageBucket: "osori-7e197.appspot.com",
+  messagingSenderId: "311080843165"
+};
+firebase.initializeApp(config);
+
 
 ReactDOM.render(
   <Router>
--- src/routes/SearchActivePage.js
+++ src/routes/SearchActivePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import firebase from 'firebase';
 import Header from '../components/Header/Header';
 
 
@@ -9,6 +10,18 @@ class SearchActivePage extends Component {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount() {
+    firebase.database().ref('Profiles')
+      .once('value', (snapshot) => {
+        console.log('Profiles', snapshot.val());
+      }
+    ).catch(err => {
+      console.log('Something Wrong While fetching profiles', err);
+      }
+    )
+  }
+
   render() {
     console.log('Inside SearchActivePage');
     return (
