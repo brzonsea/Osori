17d0dd89f600e7df436d9cec2c9b9bc1853e3d4e
Namung Kim <knw1121@gmail.com>
2018-05-27 23:50:41 +0900

    Added Profile picture loading via Google Custom Search Api

--- src/components/Profile/ProfileMain/ProfileMain.js
+++ src/components/Profile/ProfileMain/ProfileMain.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import GoogleImages from 'google-images';
 import { exampleProfile } from '../../../assets/images';
 import './ProfileMain.css';
+
+const API_KEY = 'AIzaSyC8L-IC-hN1-5TDxemn91lTMEc4RWHkTsI';
+const CSE_ID = '006609745617069818875:lbvun3-obqi';
+const client = new GoogleImages(CSE_ID, API_KEY);
 
 class ProfileMain extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      profilePicURL: '',
+    }
+  }
 
+  componentWillReceiveProps(nextProps) {
+    const { name } = nextProps;
+    if (name) {
+      console.log('client', client, name);
+      client.search(name)
+        .then(images => {
+          console.log('images fetched by google', images);
+          if (images.length !== 0) {
+            this.setState({ profilePicURL: images[0].thumbnail.url })
+          }
+          /*
+          [{
+          "url": "http://steveangello.com/boss.jpg",
+          "type": "image/jpeg",
+          "width": 1024,
+          "height": 768,
+          "size": 102451,
+          "thumbnail": {
+          "url": "http://steveangello.com/thumbnail.jpg",
+          "width": 512,
+          "height": 512
+        }
+      }]
+      */
+    }).catch(err => {
+      console.log('Something wrong while fetching image search ', err);
+    });
     }
   }
 
@@ -14,22 +49,23 @@ class ProfileMain extends Component {
     return(
       <div className="Profile-Main-Container">
         <div className="Profile-picture-container">
-          <img src={exampleProfile} className="Profile-picture" />
+          <img src={this.state.profilePicURL} className="Profile-picture" />
         </div>
         <div className="Profile-Main-Right-Container">
           <div className="first-row">
             {this.props.name}
           </div>
           <div className="second-row">
-            {this.props.keywords.map((keyword) => {
+            {this.props.keywords && this.props.keywords.map((keyword, index) => {
+              if (index > 5) return;
               return (
                 <div className="keyword">
-                  {keyword}
+                  {`#${keyword}`}
                 </div>
               )
             })}
           </div>
-          <div className="third-row">
+          {false && <div className="third-row">
             {this.props.relatedPpl.map((keyword) => {
               return (
                 <div className="keyword">
@@ -37,7 +73,7 @@ class ProfileMain extends Component {
                 </div>
               )
             })}
-          </div>
+          </div>}
         </div>
       </div>
     );
--- src/routes/ProfilePage.js
+++ src/routes/ProfilePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
 import NavBar from '../components/NavBar/NavBar';
 import ProfileMain from '../components/Profile/ProfileMain/ProfileMain';
 import KeywordTimeline from '../components/KeywordTimeline/KeywordTimeline';
@@ -12,13 +13,35 @@ class ProfilePage extends Component {
       name: '김정은',
       keywords: ['#남북정상회담', '#한반도비핵화', '#늙다리미치광이', '#로켓맨', '#최대다섯개'],
       relatedPpl: ['@문재인', '@김여정', '@도널드트럼프', '@관련인물', '@최대다섯개'],
+      newsList: [],
+      positions: [],
+      nicknames: [],
     }
   }
 
   componentDidMount() {
     console.log('match props', this.props.match);
+    let id;
+    if (this.props.match.params.id) {
+      id = this.props.match.params.id;
+      let fetchedProfile;
+      firebase.database().ref(`Profiles/${id}`)
+        .once('value', (snapshot) => {
+          console.log('Fetched Profile', snapshot.val());
+          fetchedProfile = snapshot.val();
+          this.setState({
+            name: fetchedProfile.Name,
+            keywords: fetchedProfile.Keywords,
+            newsList: fetchedProfile.News,
+            positions: fetchedProfile.Position,
+            nicknames: fetchedProfile.Nickname,
+          })
+        }).catch(err => {
+          console.log('Something Wrong While fetching Profile', err);
+        });
+    }
   }
-  
+
   render() {
     return (
       <div>
